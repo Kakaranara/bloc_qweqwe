e63b59040a0e6687a2112a320aaca1e5883b08cd
kakaranara <74239131+Kakaranara@users.noreply.github.com>
2022-03-10 12:51:49 +0700

    generated router impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_qweqwe/logic/cubit/counter_cubit.dart';
+import 'package:bloc_qweqwe/presentation/router/app_router.dart';
 import 'package:bloc_qweqwe/presentation/screen/screen1.dart';
 import 'package:bloc_qweqwe/presentation/screen/screen2.dart';
 import 'package:bloc_qweqwe/presentation/screen/screen3.dart';
@@ -18,40 +19,18 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final CounterCubit _counterCubit = CounterCubit();
+  final AppRoute _appRoute = AppRoute();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print("--------------- BUILD METHOD ---------------");
+    debugPrint("--------------- BUILD METHOD ---------------");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      routes: {
-        '/screen1': (context) =>
-            BlocProvider.value(
-              value: _counterCubit,
-              child: Screen1(
-                title: "screen1",
-                color: Colors.blueAccent,
-              ),
-            ),
-        '/screen2': (context) =>
-            BlocProvider.value(
-              value: _counterCubit,
-              child: Screen2(
-                title: "Screen2",
-                color: Colors.amberAccent,
-              ),
-            ),
-        '/screen3' : (context) =>
-            BlocProvider.value(
-              value: _counterCubit,
-              child: Screen3(title: "Screen3", color: Colors.redAccent)
-            )
-      },
-      initialRoute: '/screen1',
+      onGenerateRoute: _appRoute.onGeneratedRoute,
     );
   }
   @override
---
+++ lib/presentation/router/app_router.dart
@@ -0,0 +1,43 @@
+import 'package:bloc_qweqwe/logic/cubit/counter_cubit.dart';
+import 'package:bloc_qweqwe/presentation/screen/screen1.dart';
+import 'package:bloc_qweqwe/presentation/screen/screen2.dart';
+import 'package:bloc_qweqwe/presentation/screen/screen3.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppRoute {
+  final CounterCubit _counterCubit = CounterCubit();
+
+  Route? onGeneratedRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case '/':
+        return MaterialPageRoute(
+            builder: (_) =>
+                BlocProvider.value(
+                  value: _counterCubit,
+                  child: Screen1(title: "Screen1", color: Colors.blueAccent),
+                ));
+      case '/screen2':
+        return MaterialPageRoute(
+            builder: (_) =>
+                BlocProvider.value(
+                  value: _counterCubit,
+                  child: Screen2(title: "Screen2", color: Colors.yellowAccent),
+                ));
+      case '/screen3':
+        return MaterialPageRoute(
+            builder: (_) =>
+                BlocProvider.value(
+                  value: _counterCubit,
+                  child: Screen3(title: "Screen3", color: Colors.redAccent),
+                ));
+      default:
+        return null;
+    }
+  }
+
+  void dispose() {
+    _counterCubit.close();
+  }
+}
